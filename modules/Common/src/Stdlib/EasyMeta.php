24e59544aedcd428f6a557f6d36b59435702fe62
Kahani Ploessl <kahani@Kahanis-MBP.ht.home>
2024-04-18 12:25:33 -0300

    Common Update

--- modules/Common/src/Stdlib/EasyMeta.php
+++ modules/Common/src/Stdlib/EasyMeta.php
@@ -293,6 +293,11 @@ class EasyMeta
     /**
      * @var array
      */
+    protected static $resourceTemplateClassesByIds;
+
+    /**
+     * @var array
+     */
     protected static $vocabularyIdsByPrefixes;
 
     /**
@@ -488,6 +493,31 @@ class EasyMeta
     }
 
     /**
+     * Get the main data types ("literal", "resource", or "uri") of names.
+     *
+     * @param string|null $name A name.
+     * @return string|null The main data type names matching the names.
+     */
+    public function dataTypeMains($dataTypes = null): array
+    {
+        static $dataTypesMains;
+        if (!is_array($dataTypesMains)) {
+            $dataTypesMains = [];
+            foreach ($this->dataTypeNames() as $dataType) {
+                $dataTypesMains[$dataType] = $this->dataTypeMain($dataType);
+            }
+        }
+        if ($dataTypes === null) {
+            return $dataTypesMains;
+        }
+        if (is_scalar($dataTypes)) {
+            $dataTypes = [$dataTypes];
+        }
+        // TODO Keep order.
+        return array_intersect_key($dataTypesMains, array_flip($dataTypes));
+    }
+
+    /**
      * Get the main type of the custom vocab: "literal", "resource" or "uri".
      *
      * @todo Check for dynamic custom vocabs.
@@ -653,10 +683,10 @@ class EasyMeta
      */
     public function propertyLabels($termsOrIds = null): array
     {
+        if (is_null(static::$propertyLabelsByTerms)) {
+            $this->initProperties();
+        }
         if (is_null($termsOrIds)) {
-            if (is_null(static::$propertyLabelsByTerms)) {
-                $this->initProperties();
-            }
             return static::$propertyLabelsByTerms;
         }
         if (is_scalar($termsOrIds)) {
@@ -790,10 +820,10 @@ class EasyMeta
      */
     public function resourceClassLabels($termsOrIds = null): array
     {
+        if (is_null(static::$resourceClassLabelsByTerms)) {
+            $this->initResourceClasses();
+        }
         if (is_null($termsOrIds)) {
-            if (is_null(static::$resourceClassLabelsByTerms)) {
-                $this->initResourceClasses();
-            }
             return static::$resourceClassLabelsByTerms;
         }
         if (is_scalar($termsOrIds)) {
@@ -896,6 +926,50 @@ class EasyMeta
         }
         // TODO Keep original order.
         return array_intersect_key(static::$resourceTemplateLabelsByLabelsAndIds, array_flip($labelsOrIds));
+    }
+
+    /**
+     * Get a resource template class by label or id.
+     *
+     * @param int|string|null $labelOrId A id or a label.
+     * @return string|null The resource class matching label or id.
+     */
+    public function resourceTemplateClassId($labelOrId): ?int
+    {
+        $templateId = $this->resourceTemplateId($labelOrId);
+        if (!$templateId) {
+            return null;
+        }
+        $classIds = $this->resourceTemplateClassIds();
+        return $classIds[$templateId] ?? null;
+    }
+
+    /**
+     * Get one or more resource template class ids by labels or by numeric ids.
+     *
+     * @param array|int|string|null $labelsOrIds One or multiple ids or labels.
+     * @return int[] The resource template class ids matching labels or ids, or
+     * all resource template class ids. When the input contains labels and ids
+     * matching the same templates, they are mixed.
+     * @todo Return list by labels and ids together like other (but this method is rarely used).
+     */
+    public function resourceTemplateClassIds($labelsOrIds = null): array
+    {
+        if (is_null(static::resourceTemplateClassesByIds)) {
+            $this->initResourceTemplateClasses();
+        }
+        if (is_null($labelsOrIds)) {
+            return static::resourceTemplateClassesByIds;
+        }
+        if (is_scalar($labelsOrIds)) {
+            $labelsOrIds = [$labelsOrIds];
+        }
+        $templateIds = $this->resourceTemplateIds($labelsOrIds);
+        if (!$templateIds) {
+            return [];
+        }
+        // TODO Keep original order.
+        return array_intersect_key(static::resourceTemplateClassesByIds, array_flip($labelsOrIds));
     }
 
     /**
@@ -1253,6 +1327,21 @@ SQL;
         $resourceTemplateIdsByLabels = array_map('intval', $result);
         $resourceTemplateIdsByIds = array_combine($resourceTemplateIdsByLabels, $resourceTemplateIdsByLabels);
         static::$resourceTemplateIdsByLabelsAndIdsUsed = $resourceTemplateIdsByLabels + $resourceTemplateIdsByIds;
+    }
+
+    protected function initResourceTemplateClasses(): void
+    {
+        $qb = $this->connection->createQueryBuilder();
+        $qb
+            ->select(
+                'resource_template.id AS id',
+                'resource_template.resource_class_id AS class_id'
+            )
+            ->from('resource_template', 'resource_template')
+            ->orderBy('resource_template.id', 'asc')
+        ;
+        $result = $this->connection->executeQuery($qb)->fetchAllKeyValue();
+        static::$resourceTemplateClassesByIds = array_map(fn ($v) => $v ? null : (int) $v, $result);
     }
 
     protected function initVocabularies(): void
